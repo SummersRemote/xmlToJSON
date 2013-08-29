f7678e18a804515521e62cda4204d6e4a71f076d
metatribal <metatribal@users.noreply.github.com>
2013-08-29 10:52:24 -0600

    updated cdata handling, options
    when not processing cdata, merge text blocks
    when processing cdata, text content is now an array
    added options to support whitespace preferences trim, normalize

--- xmlToJSON.js
+++ xmlToJSON.js
@@ -2,17 +2,21 @@ var xmlToJSON = {};									// create a namespace
 
 xmlToJSON.Parser = (function() {							// declare the object, Parser
 
-	options = {									// default options
-		 namespaceKey : '_ns',							//   namespaces,
-		 attributeKey : '_at',							//   attributes, and
-		 textKey : '_t',							//   text,
-		 valueKey : '_v',							//   attribute values, and
-		 cdataKey : '_c'							//   cdata
+
+	// options
+	options = {
+		 parseCDATA : false,						//   extract cdata blocks, results in an array of text and cdata blocks
+		 trim : false,							//   trim leading and trailing whitespace in text nodes
+		 normalize: true,						//	 collapse multiple spaces to single space
+		 namespaceKey : '_ns',						//   namespaces,
+		 attributeKey : '_at',						//   attributes, and
+		 textKey : '_t',						//   text,
+		 valueKey : '_v',						//   attribute values, and
 	 }
 
-    var parserConstructor = function Parser(opt) {					// with a constructor to return (see end of function)
+    var parserConstructor = function Parser(opt) {				// with a constructor to return (see end of function)
         if(false === (this instanceof Parser)) {
-            return new Parser();							// it doesn't do anything, but gives an object to provide method upon
+            return new Parser();						// it doesn't do anything, but gives an object to provide method upon
         }
 
 		//initialize options
@@ -23,7 +27,7 @@ xmlToJSON.Parser = (function() {							// declare the object, Parser
 
     parserConstructor.prototype.parseString = function (xmlString) {			// such as this one!  add parseString method to the Parser object
 
-			parser = sax.parser(true, {xmlns: true, trim: true});		// get an instance of the sax.js parser
+			parser = sax.parser(true, {xmlns: true, trim: options.trim, normalize : options.normalize});		// get an instance of the sax.js parser
 			stack = [];							// create an empty array to hold and collapse objects
 
 			// sax.js functions begin here
@@ -92,19 +96,38 @@ xmlToJSON.Parser = (function() {							// declare the object, Parser
 				if (stack.length > 0) {					// make sure there is at least one object on the stack
 					nodeTmp = stack.pop();				// get the object
 					for(var key in nodeTmp) break;			// get it's tag name
-					nodeTmp[key][options.textKey] = text;		// set the text value of the node
+
+					if (options.parseCDATA) {					// if we are parsing cdata blocks
+						if (nodeTmp[key][options.textKey]) 		 	// and data exists
+							nodeTmp[key][options.textKey].push(text);	// append the new data
+						} else {
+							nodeTmp[key][options.textKey] =
+								nodeTmp[key][options.textKey] = [text];	// otherwise, add it
+						}
+					} else {
+						if (nodeTmp[key][options.textKey]) 			// if there is already some text (skipping cdata)
+							nodeTmp[key][options.textKey] =
+								nodeTmp[key][options.textKey]+ text;	// append the new text value
+						} else {
+							nodeTmp[key][options.textKey] = text;		// otherwise, just add it.
+						}
+					}
 					stack.push(nodeTmp);				// push the updated object back to the stack
 				}
 			};
 
+
 			parser.oncdata = function (cdata) {
-				if (stack.length > 0) {					// make sure there is at least one object on the stack
-					nodeTmp = stack.pop();				// get the object
-					for(var key in nodeTmp) break;			// get it's tag name
-					nodeTmp[key][options.cdataKey] = cdata;		// set the text value of the node
-					stack.push(nodeTmp);				// push the updated object back to the stack
+				if (options.parseCDATA) {
+					if (stack.length > 0) {					// make sure there is at least one object on the stack
+						nodeTmp = stack.pop();				// get the object
+						for(var key in nodeTmp) break;			// get it's tag name
+						nodeTmp[key][options.textKey].push(cdata);	// push the cdata value to the text array
+						stack.push(nodeTmp);				// push the updated object back to the stack
+					}
 				}
 			};
+
 
 			parser.onerror = function (error) {
 			  console.debug('error ' + error);				// print errors as they occur
