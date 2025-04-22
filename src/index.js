54a2b6f25671873b8a9a62dc82519337fa6ae866
William Summers <william.summers@gmail.com>
2025-04-22 14:45:39 -0500

    removed some default exports

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ export { default as XMLToJSONConverter } from './core/components/XMLToJSONConver
 export { default as JSONToXMLConverter } from './core/components/JSONToXMLConverter.js';
 export { default as PathNavigator } from './core/components/PathNavigator.js';
 export { default as SchemaGenerator } from './core/components/SchemaGenerator.js';
-export { default as BooleanTransformer } from './core/transformers/BooleanTransformer.js';
-export { default as NumberTransformer } from './core/transformers/NumberTransformer.js';
-export { default as StringReplaceTransformer } from './core/transformers/StringReplaceTransformer.js';
+// export { default as BooleanTransformer } from './core/transformers/BooleanTransformer.js';
+// export { default as NumberTransformer } from './core/transformers/NumberTransformer.js';
+// export { default as StringReplaceTransformer } from './core/transformers/StringReplaceTransformer.js';
 // export { default as ValueTransformer } from './core/transformers/ValueTransformer.js';
